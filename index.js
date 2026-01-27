7e4b2f7e412b487395b2947718962a668c8be411
jade <jadesada.jung@gmail.com>
2026-01-27 19:35:34 +0700

    Express.js 2.GET requests

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const fs = require('fs')
 
 const app = express();
 const port = 8080
 
-app.get('/hello', (req, res) => {
-    res.status(200).send("Hello world")
-})
+// app.get('/hello', (req, res) => {
+//     res.status(200).send("Hello world")
+// })
 
-app.post('/customer', (req, res) => {
-    res.status(200).json({ firstname: "Alice", age: 30 })
+// app.post('/customer', (req, res) => {
+//     res.status(200).json({ firstname: "Alice", age: 30 })
+// })
+
+let data = JSON.parse(fs.readFileSync('./students.txt', 'utf-8'))
+
+app.get('/api/v1/students', (req, res) => {
+    res.status(200).json({ status: "sucess", data })
 })
 
 
