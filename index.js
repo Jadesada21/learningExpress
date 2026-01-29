d62995c1aedb9df6b492d24092ba2d00bea7a127
jadesada <jadesada.jung@gmail.com>
2026-01-29 16:13:47 +0700

    Express.js 3.POST requests

--- index.js
+++ index.js
@@ -4,21 +4,32 @@ const fs = require('fs')
 const app = express();
 const port = 8080
 
-// app.get('/hello', (req, res) => {
-//     res.status(200).send("Hello world")
-// })
-
-// app.post('/customer', (req, res) => {
-//     res.status(200).json({ firstname: "Alice", age: 30 })
-// })
+// ใช้ express ในการอนุญาติที่จะส่ง json เข้ามา
+app.use(express.json())
 
 let data = JSON.parse(fs.readFileSync('./students.txt', 'utf-8'))
 
 app.get('/api/v1/students', (req, res) => {
-    res.status(200).json({ status: "sucess", data })
+    res.status(200).json({ status: "sucess", data: data })
 })
+
+
+app.post('/api/v1/students', (req, res) => {
+    let body = req.body
+    console.log(body)
+    let respone = data.filter((e) => e.Name == body.name)
+    res.status(200).json(respone)
+})
+
+
 
 
 app.listen(port, () => {
     console.log("Server is running on Port 8080")
 })
+
+
+
+
+
+
