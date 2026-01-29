885ea40caf96d19ebfd7c23a7a3626176a63e48a
jadesada <jadesada.jung@gmail.com>
2026-01-29 16:21:27 +0700

    Express.js 15.Static Files

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ app.use(express.json());
 // 3rd party middleware
 app.use(morgan('dev'))
 
+app.use(express.static('./public'))
+
 app.use('/api/v1/students', studentsRouters)
 app.use('/api/v1/users', usersRouters)
+
+
 
 app.listen(port, () => {
     console.log("Server is running on port 8080")
