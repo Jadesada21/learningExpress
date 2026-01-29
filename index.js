e9f5d5ce007d61080ab8dd0d55ac768c0f304847
jadesada <jadesada.jung@gmail.com>
2026-01-29 16:16:07 +0700

    Express.js 6.Delete Requests

--- index.js
+++ index.js
@@ -7,17 +7,12 @@ const port = 8080
 // ใช้ express ในการอนุญาติที่จะส่ง json เข้ามา
 app.use(express.json())
 
-let data = JSON.parse(fs.readFileSync('./students.txt', 'utf-8'))
 
-app.patch('/api/v1/students/:id', (req, res) => {
-    if (!req.params.id) {
-        res.status(400).json({ status: "Fail", message: "bad request" })
-    }
-
-    res.status(200).json({ status: "Success", data: "Update successfully" })
+app.delete('/api/v1/students/:id', (req, res) => {
+    let id = req.params.id
+    console.log(id);
+    res.status(200).json({ status: "success", data: "Delete Successfully" })
 })
-
-
 
 
 app.listen(port, () => {
