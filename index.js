e1d250f69aec8007d3b6a636ebb233762388ff20
jadesada <jadesada.jung@gmail.com>
2026-01-29 16:16:31 +0700

    Express.js 7.Refactoring

--- index.js
+++ index.js
@@ -4,15 +4,62 @@ const fs = require('fs')
 const app = express();
 const port = 8080
 
-// ใช้ express ในการอนุญาติที่จะส่ง json เข้ามา
+
 app.use(express.json())
 
+let data = JSON.parse(fs.readFileSync('./students.txt', 'utf-8'))
 
-app.delete('/api/v1/students/:id', (req, res) => {
+const getStudents = (req, res) => {
+    res.status(200).json({ status: "success", data: data })
+}
+
+const getStudentsWithParam = (req, res) => {
+    let id = req.params.id
+    let response = data.find((e) => e.ID == id)
+    if (response) {
+        res.status(200).json({ status: "success", data: response })
+    } else {
+        res.status(404).json({ status: "Fail", message: "data not found" })
+    }
+}
+
+const createStudents = (req, res) => {
+    let body = req.body
+    let response = data.filter((e) => e.Name == body.name)
+    res.status(200).json({ status: "success", data: response })
+}
+
+const updateStudent = (req, res) => {
+    if (!req.params.id) {
+        res.status(400).json({ status: "Fail", message: "bad request" })
+    }
+    res.status(200).json({ status: "success", data: "Update successfully" })
+}
+
+const deleteStudents = (req, res) => {
     let id = req.params.id
     console.log(id);
     res.status(200).json({ status: "success", data: "Delete Successfully" })
-})
+}
+
+// better practice
+
+// app.get('/api/v1/students', getStudents)
+
+// app.post('/api/v1/students', createStudents)
+
+// app.get('/api/v1/students/:id', getStudentsWithParam)
+
+// app.patch('/api/v1/students/:id', updateStudent)
+
+// app.delete('/api/v1/students/:id', deleteStudents)
+
+
+// best practice
+app.route('/api/v1/students').get(getStudents).post(createStudents)
+
+app.route('/api/v1/students/:id').get(getStudentsWithParam)
+    .patch(updateStudent).delete(deleteStudents)
 
 
 app.listen(port, () => {
